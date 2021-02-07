e2f11a49c2d8eadeb4f02808b04530b55d3d5fc1
Rose <rose.barmani@polymtl.ca>
2021-02-07 17:08:45 -0500

    Get battery level from drone

--- server/server/main.py
+++ server/server/main.py
@@ -2,5 +2,36 @@
 """
 Main module.
 """
+import time
+import cflib
+from connection_crazyflie import ConnectionCrazyflie
 
 print('Hello world!')
+
+#setup connection
+#setup logging
+#get battery
+
+if __name__ == '__main__':
+
+    cflib.crtp.init_drivers(enable_debug_driver=False)
+
+    print('Scanning interfaces for Crazyflies...')
+    available = cflib.crtp.scan_interfaces()
+
+    print('Crazyflies found:')
+    for i in available:
+        print(i[0])
+
+    crazyflies:list = []
+    if len(available) > 0:
+        for crazyflie in available:
+            crazyflies.append(ConnectionCrazyflie(crazyflie[0]))
+    else:
+        print('No Crazyflies found, cannot run example')
+
+    # The Crazyflie lib doesn't contain anything to keep the application alive,
+    # so this is where your application should do something. In our case we
+    # are just waiting until we are disconnected.
+    while crazyflies[0].is_connected:
+        time.sleep(1)

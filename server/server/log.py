79cedc6ad4a6535f68f77503e6a3723682772e06
moumou <moumouyasmine@gmail.com>
2021-02-07 19:08:39 -0500

    Format battery level

--- server/server/log.py
+++ server/server/log.py
@@ -8,8 +8,8 @@ class Log:
     def __init__(self, crazyflie: Crazyflie):
         self._crazyflie = crazyflie
         self._lg = LogConfig(name = 'DroneData', period_in_ms = PERIOD)
-        self._lg.add_variable('pm.batteryLevel')
-
+        self._lg.add_variable('pm.vbat')
+        
     def start_logging(self):
         try:
             self._crazyflie.log.add_config(self._lg)
@@ -24,7 +24,12 @@ class Log:
             print('Could not add log configuration')
 
     def log_data(self, timestamp, data, logconf):
-        print('[%d][%s]: %s' % (timestamp, logconf.name, data))
+        # Temporary diplay of battery TO DO: change firmware to send formatted battery?
+        batteryLevel = (data['pm.vbat'] - 3.0)/ (4.23 - 3.0) * 100.0
+        print('Battery Level: %.2f' % (batteryLevel)) 
+        
+        # To print all log variables from log configuration     
+        #print('[%d][%s]: %s' % (timestamp, logconf.name, data))
 
     def log_error(self, timestamp, msg, logconf):
         print('[%d]: Logging error of %d: %s'% (timestamp, logconf.name, msg))

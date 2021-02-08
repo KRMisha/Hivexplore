0b56f9c8e885d80bd81269db2ce97b25be4bd06f
moumou <moumouyasmine@gmail.com>
2021-02-08 18:46:31 -0500

    Modify Log to use log configuration wrappers

--- server/server/log.py
+++ server/server/log.py
@@ -1,43 +1,30 @@
 from cflib.crazyflie import Crazyflie
 from cflib.crazyflie.log import LogConfig
-
-PERIOD = 1000
+from log_configurations.log_config_wrapper import LogConfigWrapper 
+from log_configurations.log_health import LogHealth
+from log_configurations.log_stabilizer import LogStabilizer
 
 class Log:
 
     def __init__(self, crazyflie: Crazyflie):
         self._crazyflie = crazyflie
-        self._lg = LogConfig(name = 'DroneData', period_in_ms = PERIOD)
-        self._lg.add_variable('pm.vbat')
+
+        self._log_config_wrappers = []
+        self._log_config_wrappers.append(LogHealth()) 
+        self._log_config_wrappers.append(LogStabilizer())
 
     def start_logging(self):
         try:
-            self._crazyflie.log.add_config(self._lg)
+            for config_wrapper in self._log_config_wrappers:
+                log_config = config_wrapper._logConfig
 
-            self._lg.data_received_cb.add_callback(self.log_data)
-            self._lg.error_cb.add_callback(self.log_error)
-            self._lg.start()
+                self._crazyflie.log.add_config(log_config)
+                log_config.data_received_cb.add_callback(config_wrapper.log_data)
+                log_config.error_cb.add_callback(config_wrapper.log_error)
+                log_config.start()
+        
         except KeyError as e:
             print('Could not start logging data,'
                   '{} was not found in TOC'.format(str(e)))
-        except AttributeError:
-            print('Could not add log configuration')
-
-    def log_data(self, timestamp, data, logconf):
-        battery_level = self.format_battery_level(data['pm.vbat'])
-        print('Battery Level: %.2f' % (battery_level))
-
-        # To print all log variables from log configuration
-        # print('[%d][%s]: %s' % (timestamp, logconf.name, data))
-
-    def log_error(self, timestamp, msg, logconf):
-        print('[%d]: Logging error of %d: %s'% (timestamp, logconf.name, msg))
-
-    def format_battery_level(self, voltage):
-        # Temporary diplay of battery TO DO: change firmware to send formatted battery?
-        battery_level = (voltage - 3.0)/ (4.23 - 3.0) * 100.0
-        if battery_level < 0.0:
-            battery_level = 0.0
-        elif battery_level > 100.0:
-            battery_level = 100.0
-        return battery_level
+        except AttributeError as e:
+            print('Could not add log configuration,''error: {}'.format(str(e)))

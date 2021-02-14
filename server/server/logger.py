964f409c8cd5ab0f04ac48b8943f8a0abe64a155
Misha Krieger-Raynauld <mkriegerraynauld@gmail.com>
2021-02-13 22:34:18 -0500

    Rename variables in Logger

--- server/server/logger.py
+++ server/server/logger.py
@@ -6,13 +6,13 @@ from server.log_configurations.stabilizer_log_config import StabilizerLogConfig
 class Logger:
     def __init__(self, crazyflie: Crazyflie):
         self._crazyflie = crazyflie
-        self._log_config_wrappers = [BatteryLogConfig(), StabilizerLogConfig()]
+        self._log_configs = [BatteryLogConfig(), StabilizerLogConfig()]
 
     def start_logging(self):
         try:
-            for log_config_wrapper in self._log_config_wrappers:
-                self._crazyflie.log.add_config(log_config_wrapper.log_config)
-                log_config_wrapper.start()
+            for log_config in self._log_configs:
+                self._crazyflie.log.add_config(log_config.log_config)
+                log_config.start()
         except KeyError as key_error:
             print(f'Could not start logging data, {key_error} was not found in the Crazyflie TOC')
         except AttributeError as attribute_error:

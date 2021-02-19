a4edaa114e9c8599c1784512e7b5e5c0e36f92ec
Misha Krieger-Raynauld <mkriegerraynauld@gmail.com>
2021-02-18 21:38:50 -0500

    Remove unnecessary endless loop in server

--- server/server/core/server.py
+++ server/server/core/server.py
@@ -1,11 +1,10 @@
-import time
 import logging
 from server.core.crazyflie_manager import CrazyflieManager
 from server.core.socket_server import SocketServer
 from server.core.map_generator import MapGenerator
 
 # Only output errors from the logging framework
-logging.basicConfig(level=logging.ERROR) # TODO: Investigate more liberal logging levels
+logging.basicConfig(level=logging.ERROR)
 
 
 class Server:
@@ -17,10 +16,3 @@ class Server:
     def start(self):
         self._crazyflie_manager.start()
         self._socket_server.serve()
-
-        while True:
-            time.sleep(1)
-            # TODO: Update map every second
-            # self._map_generator.update()
-
-    # TODO: Communication callbacks between the managers

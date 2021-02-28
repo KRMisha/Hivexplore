f2bf3e6275d3d0a7052abd5bef2a833130682658
Samer Massaad <samer.massaad@gmail.com>
2021-02-27 22:14:43 -0500

    Rename and reorganize variables

--- drone/app_api/src/app_main.c
+++ drone/app_api/src/app_main.c
@@ -51,113 +51,114 @@
 #define DEBUG_MODULE "APPAPI"
 
 static bool isM1LedOn = true;
-static setpoint_t setPoint;
-enum state {IDLE = 0 , STARTUP, TAKEOFF, FORWARD, LANDING, LANDED} typedef state;
-static state currentState = IDLE;
+enum state { IDLE = 0, STARTUP, TAKEOFF, FORWARD, LANDING, LANDED } typedef state;
 
-float cruiseHeight = 1.f;
-float cruiseVelocity = 0.2f;
+static const uint16_t OBTACLE_DETECTED_THRESHOLD = 300;
+const float EXPLORATION_HEIGHT = 0.5f;
+const float CRUISE_VELOCITY = 0.2f;
 
-static void setWaypoint(setpoint_t* setpoint, float vx, float vy, float z, float yawrate) {
-    setpoint->mode.z = modeAbs;
-    setpoint->position.z = z;
+static void setWaypoint(setpoint_t* setPoint, float forwardVelocity, float leftVelocity, float height, float yawRate) {
+    setPoint->mode.yaw = modeVelocity;
+    setPoint->attitudeRate.yaw = yawRate;
 
-    setpoint->mode.yaw = modeVelocity;
-    setpoint->attitudeRate.yaw = yawrate;
+    setPoint->velocity_body = true;
+    setPoint->mode.x = modeVelocity;
+    setPoint->mode.y = modeVelocity;
+    setPoint->velocity.x = forwardVelocity;
+    setPoint->velocity.y = leftVelocity;
 
-    setpoint->mode.x = modeVelocity;
-    setpoint->mode.y = modeVelocity;
-    setpoint->velocity.x = vx;
-    setpoint->velocity.y = vy;
-
-    setpoint->velocity_body = true;
+    setPoint->mode.z = modeAbs;
+    setPoint->position.z = height;
 }
-
 
 void appMain() {
     logVarId_t idUp = logGetVarId("range", "up");
     logVarId_t idDown = logGetVarId("range", "zrange");
+    logVarId_t idFront = logGetVarId("range", "front");
     // logVarId_t idLeft = logGetVarId("range", "left");
     // logVarId_t idRight = logGetVarId("range", "right");
-    logVarId_t idFront = logGetVarId("range", "front");
     // logVarId_t idBack = logGetVarId("range", "back");
 
+    // TODO: Check module status and switch to Out-Of-Service state if unavailale
     // paramVarId_t idPositioningDeck = paramGetVarId("deck", "bcFlow2");
     // paramVarId_t idMultiranger = paramGetVarId("deck", "bcMultiranger");
 
+    float forwardVelocity = 0;
+    float leftVelocity = 0;
+    float height = 0;
+    float yawRate = 0;
+    static setpoint_t setPoint;
+    static state currentState = IDLE;
     vTaskDelay(M2T(1000));
-    currentState = IDLE;
     while (true) {
         vTaskDelay(M2T(10));
         // uint8_t positioningInit = paramGetUint(idPositioningDeck);
         // uint8_t multirangerInit = paramGetUint(idMultiranger);
         uint16_t up = logGetUint(idUp);
-        // DEBUG_PRINT("Up: %i\n", up);
         uint16_t down = logGetUint(idDown);
         // uint16_t left = logGetUint(idLeft);
         // uint16_t right = logGetUint(idRight);
         uint16_t front = logGetUint(idFront);
         // uint16_t back = logGetUint(idBack);
-
         switch (currentState) {
-            case IDLE: {
-                    if (up < 200) {
-                        DEBUG_PRINT("Startup\n");
-                        currentState = STARTUP;
-                    }
-                    memset(&setPoint, 0, sizeof(setpoint_t));
-                    commanderSetSetpoint(&setPoint, 3);
-                }
-                break;
-            case STARTUP: {
-                    if (up > 300) {
-                        DEBUG_PRINT("Takeoff\n");
-                        currentState = TAKEOFF;
-                    }
-                }
-                break;
-            case TAKEOFF: {
-                    // TODO: Check obstacle above
-                    float vx = 0.0f;
-                    float vy = 0.0f;
-                    setWaypoint(&setPoint, vx, vy, cruiseHeight, 0);
-                    commanderSetSetpoint(&setPoint, 3);
-                    if(down >= cruiseHeight * 1000) {
-                        DEBUG_PRINT("Take off started\n");
-                        currentState = FORWARD;
-                        DEBUG_PRINT("Take off finished\n");
-                    }
-                }
-                break;
-            case FORWARD: {
-                    float vy = 0.f;
-                    setWaypoint(&setPoint, cruiseVelocity, vy, cruiseHeight, 0);
-                    commanderSetSetpoint(&setPoint, 3);
-                    if (front < 200) {
-                        DEBUG_PRINT("Wow minute madame, on se calme le ponpon\n");
-                        currentState = LANDING;
-                    }
-                }
-                break;
-            case LANDING: {
-                    float vx = 0.0f;
-                    float vy = 0.0f;
-                    float height = 0.0f;
-                    setWaypoint(&setPoint, vx, vy, height, 0);
-                    commanderSetSetpoint(&setPoint, 3);
-                    if (down < 50) {
-                        DEBUG_PRINT("Land started\n");
-                        currentState = LANDED;
-                        DEBUG_PRINT("Land finished\n");
-                    }
-                }
-                break;
-            case LANDED: {
-                    DEBUG_PRINT("4->0\n");
-                    currentState = IDLE;
-                }
-                break;
+        case IDLE: {
+            if (up < OBTACLE_DETECTED_THRESHOLD) {
+                DEBUG_PRINT("Startup\n");
+                currentState = STARTUP;
+            }
+            memset(&setPoint, 0, sizeof(setpoint_t));
+        } break;
+        case STARTUP: {
+            if (up > OBTACLE_DETECTED_THRESHOLD) {
+                DEBUG_PRINT("Takeoff\n");
+                currentState = TAKEOFF;
+            }
+        } break;
+        case TAKEOFF: {
+            // TODO: Check obstacle above
+            forwardVelocity = 0;
+            leftVelocity = 0;
+            height = EXPLORATION_HEIGHT;
+            yawRate = 0;
+            setWaypoint(&setPoint, forwardVelocity, leftVelocity, height, yawRate);
+            const uint16_t millimeterToMeterFactor = 1000;
+            if (down >= EXPLORATION_HEIGHT * millimeterToMeterFactor) {
+                DEBUG_PRINT("Take off started\n");
+                currentState = FORWARD;
+                DEBUG_PRINT("Take off finished\n");
+            }
+        } break;
+        case FORWARD: {
+            leftVelocity = 0.f;
+            forwardVelocity = CRUISE_VELOCITY;
+            height = EXPLORATION_HEIGHT;
+            yawRate = 0;
+            setWaypoint(&setPoint, forwardVelocity, leftVelocity, height, yawRate);
+            if (front < OBTACLE_DETECTED_THRESHOLD) {
+                DEBUG_PRINT("Wow minute madame, on se calme le ponpon\n");
+                currentState = LANDING;
+            }
+        } break;
+        case LANDING: {
+            forwardVelocity = 0;
+            leftVelocity = 0;
+            height = 0;
+            yawRate = 0;
+            setWaypoint(&setPoint, forwardVelocity, leftVelocity, height, yawRate);
+            const uint16_t LANDED_HEIGHT = 50;
+            if (down < LANDED_HEIGHT) {
+                DEBUG_PRINT("Land started\n");
+                currentState = LANDED;
+                DEBUG_PRINT("Land finished\n");
+            }
+        } break;
+        case LANDED: {
+            DEBUG_PRINT("4->0\n");
+            currentState = IDLE;
+        } break;
         }
+        const uint8_t taskPriority = 3;
+        commanderSetSetpoint(&setPoint, taskPriority);
     }
 }
 

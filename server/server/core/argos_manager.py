6f0d73a1c6c23a1d87b4a5012c344520ac93fa88
Simon Gauvin <gauvinsimon@gmail.com>
2021-02-28 23:47:29 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- server/server/core/argos_manager.py
+++ server/server/core/argos_manager.py
@@ -17,7 +17,7 @@ class ArgosManager:
         self._web_socket_server.bind('connect', self._new_connection_callback)
         self._web_socket_server.bind('set-led', self._set_led_enabled)
 
-        # Argos bindings
+        # ARGoS bindings
         self._unix_socket_client.bind('drone-ids', self._get_drone_ids_callback)
         self._unix_socket_client.bind('pm.batteryLevel', self._get_battery_callback)
         # TODO: Handle ['stabilizer.roll', 'stabilizer.pitch', 'stabilizer.yaw']

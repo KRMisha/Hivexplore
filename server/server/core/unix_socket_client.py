a7ab2d87b1cad5abdc95ed593cf1778be67bdf90
Rose <rose.barmani@polymtl.ca>
2021-03-01 17:42:01 -0500

    Remove unnecessary TODO for callbacks

--- server/server/core/unix_socket_client.py
+++ server/server/core/unix_socket_client.py
@@ -74,7 +74,7 @@ class UnixSocketClient:
             try:
                 message = json.loads(message_bytes.decode('utf-8'))
                 for callback in self._callbacks.get(message['logName'], []):
-                    callback(message['droneId'], message['variables']) # TODO: Fix existing callbacks till they work
+                    callback(message['droneId'], message['variables'])
             except (json.JSONDecodeError, KeyError) as exc:
                 print('UnixSocketClient error: Invalid message received:', exc)
 

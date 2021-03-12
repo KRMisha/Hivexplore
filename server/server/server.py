155dcc846bd0669ffa0d43794609d453fd602a2c
Simon Gauvin <gauvinsimon@gmail.com>
2021-03-12 17:00:28 -0500

    Change imports

--- server/server/server.py
+++ server/server/server.py
@@ -1,9 +1,9 @@
 import asyncio
 import logging
-from server.core.web_socket_server import WebSocketServer
-from server.core.map_generator import MapGenerator
 from server.managers.crazyflie_manager import CrazyflieManager
 from server.managers.argos_manager import ArgosManager
+from server.map_generator import MapGenerator
+from server.sockets.web_socket_server import WebSocketServer
 
 # Only output errors from the logging framework
 logging.basicConfig(level=logging.ERROR)

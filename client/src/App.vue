1bafce3e9169be8c683fa5b840f54feb21f11f09
Nathanael Beaudoin-Dion <nathanael.beaudoin-dion@polymtl.ca>
2021-03-17 10:54:19 -0400

    Add dialog and toasts

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,20 +1,22 @@
 <template>
+    <Toast />
+    <ConfirmDialog />
+    <ConfirmDialog group="positionDialog"></ConfirmDialog>
+
     <div class="container">
         <Map class="map-container" />
         <div class="button-container">
-            <!-- TODO: Add dialog if we had an emergency landing -->
             <Button
                 label="Start mission"
                 class="left-button"
                 :disabled="droneIds.length === 0 || missionState !== MissionState.Standby"
-                @click="setMissionState(MissionState.Exploring)"
+                @click="startMissionButtonClick()"
             />
             <Button
                 label="Return to base"
                 :disabled="droneIds.length === 0 || missionState !== MissionState.Exploring"
-                @click="setMissionState(MissionState.Returning)"
+                @click="returnToBaseButtonClick()"
             />
-            <!-- TODO: Add dialog when Emergency land -->
             <Button
                 :label="endMissionButtonLabel"
                 class="right-button"
@@ -48,6 +50,8 @@ import Drone from '@/components/Drone.vue';
 import Map from '@/components/Map.vue';
 import { SocketClient } from '@/classes/socket-client';
 import { MissionState } from '@/enums/mission-state';
+import { useConfirm } from "primevue/useconfirm";
+import { useToast } from "primevue/usetoast";
 
 export default defineComponent({
     name: 'App',
@@ -56,8 +60,11 @@ export default defineComponent({
         Map,
     },
     setup() {
+        const confirm = useConfirm();
+        const toast = useToast();
         const socketClient = new SocketClient();
         const droneIds = ref<string[]>([]);
+        let emergencyLandWasCalled = false;
 
         socketClient.bindMessage('drone-ids', (newDroneIds: string[]) => {
             droneIds.value = newDroneIds;
@@ -72,6 +79,36 @@ export default defineComponent({
             socketClient.sendMessage('mission-state', missionState);
         }
 
+        function startMissionButtonClick() {
+            // Emergency land was called use case
+            if (emergencyLandWasCalled) {
+                confirm.require({
+                    message: 'Are you sure you want to START mission since an EMERGENCY land was called?',
+                    header: 'Confirmation',
+                    icon: 'pi pi-exclamation-triangle',
+                    accept: () => {
+                        toast.add({severity:'success', summary:'Initiated', detail:'Start mission initiated', life: 3000});
+                        toast.add({severity:'success', summary:'ALATAK', detail:'ALATAK', life: 3000});
+                        setMissionState(MissionState.Exploring);
+                        emergencyLandWasCalled = false; // Reset
+                    },
+                    reject: () => {
+                        toast.add({severity:'warn', summary:'Rejected', detail:'Start mission rejected', life: 3000});
+                    }
+                });
+            // Normal use case
+            } else {
+                toast.add({severity:'success', summary:'Initiated', detail:'Start mission initiated', life: 3000});
+                toast.add({severity:'success', summary:'ALATAK', detail:'ALATAK', life: 3000});
+                setMissionState(MissionState.Exploring);
+            }
+        }
+
+        function returnToBaseButtonClick() {
+            toast.add({severity:'success', summary:'Initiated', detail:'Return to base initiated', life: 3000});
+            setMissionState(MissionState.Returning);
+        }
+
         const endMissionButtonLabel = computed((): string => {
             return (missionState.value === MissionState.Landed ? 'End mission' : 'Emergency land');
         });
@@ -81,7 +118,25 @@ export default defineComponent({
         });
 
         function endMissionButtonClick() {
-            missionState.value === MissionState.Landed ? setMissionState(MissionState.Standby) : setMissionState(MissionState.Emergency);
+            // Emergency land
+            if (missionState.value !== MissionState.Landed) {
+                confirm.require({
+                    message: 'Are you sure you want to initiate an EMERGENCY land?',
+                    header: 'Confirmation',
+                    icon: 'pi pi-exclamation-triangle',
+                    accept: () => {
+                        toast.add({severity:'success', summary:'Initiated', detail:'Emergency land initiated', life: 3000});
+                        setMissionState(MissionState.Emergency);
+                        emergencyLandWasCalled = true;
+                    },
+                    reject: () => {
+                        toast.add({severity:'warn', summary:'Rejected', detail:'Emergency land rejected', life: 3000});
+                    }
+                });
+            // End mission
+            } else {
+                setMissionState(MissionState.Standby);
+            }
         }
 
         provide('socketClient', socketClient);
@@ -95,7 +150,8 @@ export default defineComponent({
             missionState,
             missionStates: Object.values(MissionState),
             MissionState,
-            setMissionState,
+            startMissionButtonClick,
+            returnToBaseButtonClick,
             endMissionButtonLabel,
             endMissionButtonColor,
             endMissionButtonClick,
--- client/src/main.ts
+++ client/src/main.ts
@@ -3,27 +3,37 @@ import App from './App.vue';
 import Button from 'primevue/button';
 import Card from 'primevue/card';
 import Chip from 'primevue/chip';
+import ConfirmDialog from 'primevue/confirmdialog';
 import Divider from 'primevue/divider';
 import InputSwitch from 'primevue/inputswitch';
 import Knob from 'primevue/knob';
 import Panel from 'primevue/panel';
 import PrimeVue from 'primevue/config';
 import Timeline from 'primevue/timeline';
+import Toast from "primevue/toast";
+
+import ConfirmationService from 'primevue/confirmationservice';
+import ToastService from "primevue/toastservice";
 
 import 'primevue/resources/themes/md-light-indigo/theme.css';
 import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';
 
 const app = createApp(App);
-
 app.use(PrimeVue, { ripple: true });
+
+app.use(ToastService);
+app.use(ConfirmationService);
+
 app.component('Button', Button);
 app.component('Card', Card);
 app.component('Chip', Chip);
+app.component('ConfirmDialog', ConfirmDialog);
 app.component('Divider', Divider);
 app.component('InputSwitch', InputSwitch);
 app.component('Knob', Knob);
 app.component('Panel', Panel);
 app.component('Timeline', Timeline);
+app.component("Toast", Toast);
 
 app.mount('#app');

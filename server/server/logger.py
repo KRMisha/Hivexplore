0a740bf46f7da1051b0a5bba5f313fb781d902a0
Simon Gauvin <gauvinsimon@gmail.com>
2021-03-23 13:53:42 -0400

    add prints in logging functions

--- server/server/logger.py
+++ server/server/logger.py
@@ -8,9 +8,11 @@ class Logger:
         self._web_socket_server = web_socket_server
 
     def log_server_data(self, data: str):
+        print(data)
         self._web_socket_server.send_message('log', {'name': 'Server', 'message': data})
 
     def log_drone_data(self, drone_id: str, data: str):
+        print(f'{drone_id}: data')
         self._web_socket_server.send_message('log', {'name': drone_id, 'message': data})
 
     def log_map_data(self, drone_id: str, data: List[Point]):

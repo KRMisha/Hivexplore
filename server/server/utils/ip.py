f47f001a188473094eb9a22e44eb45f1811893ab
Misha Krieger-Raynauld <mkriegerraynauld@gmail.com>
2021-03-24 18:59:12 -0400

    Create get_local_ip utility function

---
+++ server/server/utils/ip.py
@@ -0,0 +1,14 @@
+import socket
+
+def get_local_ip() -> str:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        DUMMY_IP = '10.255.255.255'
+        DUMMY_PORT = 1
+        sock.connect((DUMMY_IP, DUMMY_PORT))
+        ip_address = sock.getsockname()[0]
+    except OSError:
+        ip_address = '127.0.0.1'
+    finally:
+        sock.close()
+    return ip_address

e4d1f3739107211f56a6c76add1a027634eb3c3a
Nathanael Beaudoin-Dion <nathanael.beaudoin-dion@polymtl.ca>
2021-03-23 21:33:01 -0400

    Renamings

--- client/src/utils/local-timestamp.ts
+++ client/src/utils/local-timestamp.ts
@@ -1,5 +1,5 @@
 export function getLocalTimestamp(): string {
-    const timestampUtc = new Date();
-    const timestampLocalUnfiltered = new Date(timestampUtc.getTime() - timestampUtc.getTimezoneOffset() * 60 * 1000);
-    return timestampLocalUnfiltered.toISOString().replace('Z', ''); // Remove the trailing Z since the timestamp is not in UTC
+    const utcTimestamp = new Date();
+    const localTimestamp = new Date(utcTimestamp.getTime() - utcTimestamp.getTimezoneOffset() * 60 * 1000);
+    return localTimestamp.toISOString().replace('Z', ''); // Remove the trailing Z since the timestamp is not in UTC
 }

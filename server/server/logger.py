c09df2197f372226bd69a46174061ef5f977a316
Nathanael Beaudoin-Dion <nathanael.beaudoin-dion@polymtl.ca>
2021-03-27 15:16:06 -0400

    Implement basic logger config

--- server/server/logger.py
+++ server/server/logger.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import logging
 from typing import List, TYPE_CHECKING
 from server.tuples import Point
 if TYPE_CHECKING:
@@ -15,19 +16,19 @@ class Logger:
 
     @staticmethod
     def log_server_local_data(data: str):
-        print(data)
+        logging.info(data)
 
     def log_server_data(self, data: str):
-        print(data)
+        logging.info(data)
         self._web_socket_server.send_message('log', {'name': 'Server', 'message': data})
 
     def log_drone_data(self, drone_id: str, data: str):
-        print(f'{drone_id}: {data}')
+        logging.info(f'{drone_id}: {data}')
         self._web_socket_server.send_message('log', {'name': drone_id, 'message': data})
 
     def log_map_data(self, drone_id: str, data: List[Point]):
         if len(data) == 0:
             return
         message = f'Points detected by drone {drone_id}: {data}'
-        print(message)
+        logging.info(message)
         self._web_socket_server.send_message('log', {'name': 'Map', 'message': message})
--- server/server/main.py
+++ server/server/main.py
@@ -1,9 +1,25 @@
+from datetime import datetime
+import logging
+from logging.handlers import RotatingFileHandler
+from logging import StreamHandler
 import sys
 from server.server import Server
 
 
 def main():
     try:
+        timestamp = datetime.now().isoformat().replace(':', '')
+        file_handler = RotatingFileHandler(filename=f'logs/hivexplore_logs_{timestamp}.log', maxBytes=512*1024, backupCount=10)
+        stream_handler = StreamHandler()
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format='%(asctime)s [%(levelname)s]\t%(message)s',
+            handlers=[
+                file_handler,
+                stream_handler,
+            ]
+        )
+
         is_argos_simulation = 'argos' in sys.argv[1:]
         enable_debug_driver = 'debug' in sys.argv[1:]
         server = Server(is_argos_simulation, enable_debug_driver)

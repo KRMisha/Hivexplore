54df02c4e8ac3e47a82a07d60d6ac862ec6ffd4c
Nathanael Beaudoin-Dion <nathanael.beaudoin-dion@polymtl.ca>
2021-03-28 17:57:49 -0400

    Implement changes from MR comments

--- server/server/logger.py
+++ server/server/logger.py
@@ -3,46 +3,46 @@ from __future__ import annotations
 from datetime import datetime
 import logging
 from logging import config
-from pathlib import Path
 from typing import List, TYPE_CHECKING
 import yaml
 from server.tuples import Point
 if TYPE_CHECKING:
     from server.sockets.web_socket_server import WebSocketServer
 
-timestamp = datetime.now().isoformat().replace(':', '')
-log_filename = Path('logs') / Path(f'hivexplore_logs_{timestamp}.log')
-logger = logging.getLogger('hivexplore')
+log_filename = f'logs/hivexplore_logs_{datetime.now().isoformat().replace(":", "")}.log'
 
 
-def setup_logger():
-    with open(Path('server') / Path('logging_config.yml'), 'r') as stream:
-        my_config = yaml.load(stream, Loader=yaml.FullLoader)
-        config.dictConfig(my_config)
+class DebugInfoFilter(logging.Filter):
+    @staticmethod
+    def filter(record):
+        return record.levelno in (logging.DEBUG, logging.INFO)
 
 
 class Logger:
     def __init__(self):
         self._web_socket_server: WebSocketServer
+        with open('server/logging_config.yml', 'r') as file:
+            config_dict = yaml.load(file, Loader=yaml.FullLoader)
+            config.dictConfig(config_dict)
+        self._logger = logging.getLogger('hivexplore')
 
     def set_web_socket_server(self, web_socket_server: WebSocketServer):
         self._web_socket_server = web_socket_server
 
-    @staticmethod
-    def log_server_local_data(data: str):
-        logger.info(data)
+    def log_server_local_data(self, data: str):
+        self._logger.info(data)
 
     def log_server_data(self, data: str):
-        logger.info(data)
+        self._logger.info(data)
         self._web_socket_server.send_message('log', {'name': 'Server', 'message': data})
 
     def log_drone_data(self, drone_id: str, data: str):
-        logger.info(drone_id + ': ' + data)
+        self._logger.info(drone_id + ': ' + data)
         self._web_socket_server.send_message('log', {'name': drone_id, 'message': data})
 
     def log_map_data(self, drone_id: str, data: List[Point]):
         if len(data) == 0:
             return
         message = f'Points detected by drone {drone_id}: {data}'
-        logger.info(message)
+        self._logger.info(message)
         self._web_socket_server.send_message('log', {'name': 'Map', 'message': message})
--- server/server/main.py
+++ server/server/main.py
@@ -1,11 +1,9 @@
 import sys
-from server.logger import setup_logger
 from server.server import Server
 
 
 def main():
     try:
-        setup_logger()
         is_argos_simulation = 'argos' in sys.argv[1:]
         enable_debug_driver = 'debug' in sys.argv[1:]
         server = Server(is_argos_simulation, enable_debug_driver)

954ca2d0d07a2241c38c205e33d20913296aa0a4
Nathanael Beaudoin-Dion <nathanael.beaudoin-dion@polymtl.ca>
2021-03-28 14:00:33 -0400

    Setup logger using yaml file and move setup to logger.py

--- server/server/logger.py
+++ server/server/logger.py
@@ -1,10 +1,24 @@
 from __future__ import annotations
 
+from datetime import datetime
 import logging
+from logging import config
+from pathlib import Path
 from typing import List, TYPE_CHECKING
+import yaml
 from server.tuples import Point
 if TYPE_CHECKING:
     from server.sockets.web_socket_server import WebSocketServer
+
+timestamp = datetime.now().isoformat().replace(':', '')
+log_filename = Path('logs') / Path(f'hivexplore_logs_{timestamp}.log')
+logger = logging.getLogger('hivexplore')
+
+
+def setup_logger():
+    with open(Path('server') / Path('logging_config.yml'), 'r') as stream:
+        my_config = yaml.load(stream, Loader=yaml.FullLoader)
+        config.dictConfig(my_config)
 
 
 class Logger:
@@ -16,19 +30,19 @@ class Logger:
 
     @staticmethod
     def log_server_local_data(data: str):
-        logging.info(data)
+        logger.info(data)
 
     def log_server_data(self, data: str):
-        logging.info(data)
+        logger.info(data)
         self._web_socket_server.send_message('log', {'name': 'Server', 'message': data})
 
     def log_drone_data(self, drone_id: str, data: str):
-        logging.info(f'{drone_id}: {data}')
+        logger.info(drone_id + ': ' + data)
         self._web_socket_server.send_message('log', {'name': drone_id, 'message': data})
 
     def log_map_data(self, drone_id: str, data: List[Point]):
         if len(data) == 0:
             return
         message = f'Points detected by drone {drone_id}: {data}'
-        logging.info(message)
+        logger.info(message)
         self._web_socket_server.send_message('log', {'name': 'Map', 'message': message})
--- server/server/main.py
+++ server/server/main.py
@@ -1,27 +1,11 @@
-from datetime import datetime
-import logging
-# TODO: Remove me, but bug from https://stackoverflow.com/questions/64951836/python-logging-attributeerror-module-logging-has-no-attribute-handlers
-from logging.handlers import RotatingFileHandler
-from logging import StreamHandler
-from pathlib import Path
 import sys
+from server.logger import setup_logger
 from server.server import Server
 
 
 def main():
     try:
-        timestamp = datetime.now().isoformat().replace(':', '')
-        file_handler = RotatingFileHandler(filename=Path('logs') / Path(f'hivexplore_logs_{timestamp}.log'), maxBytes=512*1024, backupCount=10)
-        stream_handler = StreamHandler()
-        logging.basicConfig(
-            level=logging.INFO,
-            format='%(asctime)s [%(levelname)s]\t%(message)s',
-            handlers=[
-                file_handler,
-                stream_handler,
-            ]
-        )
-
+        setup_logger()
         is_argos_simulation = 'argos' in sys.argv[1:]
         enable_debug_driver = 'debug' in sys.argv[1:]
         server = Server(is_argos_simulation, enable_debug_driver)

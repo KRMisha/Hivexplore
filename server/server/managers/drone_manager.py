86b00145e61837dd74c46705aac737e76a443ff3
Nathanael Beaudoin-Dion <nathanael.beaudoin-dion@polymtl.ca>
2021-03-28 21:26:07 -0400

    Add underscore for mission count in logger filename

--- server/server/managers/drone_manager.py
+++ server/server/managers/drone_manager.py
@@ -144,7 +144,7 @@ class DroneManager(ABC):
 
         if self._mission_state == MissionState.Standby:
             self._mission_count += 1
-            log_filename = f'logs/hivexplore_{datetime.now().isoformat().replace(":", "")}_mission{self._mission_count}.log'
+            log_filename = f'logs/hivexplore_{datetime.now().isoformat().replace(":", "")}_mission_{self._mission_count}.log'
             self._logger.setup(log_filename)
 
     def _set_led_enabled(self, drone_id: str, is_enabled: bool):

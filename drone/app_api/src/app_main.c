6f693d4724937574dd97f8a62eb3b7b47645254d
Rose <rose.barmani@polymtl.ca>
2021-04-01 00:50:23 -0400

    Add return and explore watchdogs

--- drone/app_api/src/app_main.c
+++ drone/app_api/src/app_main.c
@@ -46,6 +46,7 @@
 #include "app_channel.h"
 #include "commander.h"
 #include "sitaw.h"
+#include "time.h"
 
 #include "app_main.h"
 
@@ -59,12 +60,12 @@
 // Constants
 static const uint16_t OBSTACLE_DETECTED_THRESHOLD = 300;
 static const uint16_t EDGE_DETECTED_THRESHOLD = 400;
-static const float EXPLORATION_HEIGHT = 0.5f; // 0.5f
-static const float CRUISE_VELOCITY = 0.1f; // 0.2f
-static const float MAXIMUM_VELOCITY = 0.5f; // 0.7f
+static const float EXPLORATION_HEIGHT = 0.2f; // 0.5f
+static const float CRUISE_VELOCITY = 0.07f; // 0.2f
+static const float MAXIMUM_VELOCITY = 0.4f; // 0.7f
 static const uint16_t METER_TO_MILLIMETER_FACTOR = 1000;
-static const uint16_t MAXIMUM_EXPLORE_TICKS = 200;
-static const uint16_t STABILIZE_READING_TICKS = 400;
+static const uint16_t MAXIMUM_RETURN_TICKS = 800;
+static const uint16_t STABILIZE_READING_TICKS = 600;
 static const uint16_t MAXIMUM_RSSI_READINGS = 10;
 
 // States
@@ -111,11 +112,16 @@ static float targetHeight;
 static float targetYawRate;
 static float targetYawToBase;
 
-static uint16_t exploreWatchdog = MAXIMUM_EXPLORE_TICKS;
+static uint16_t returnWatchdog = MAXIMUM_RETURN_TICKS;
+static uint16_t maximumExploreTicks;
+static uint16_t exploreWatchdog;
 static uint16_t obstacleClearedCounter = STABILIZE_READING_TICKS;
 
 void appMain(void) {
     vTaskDelay(M2T(3000));
+
+    // Use current time as seed for random generator
+    srand(time(0));
 
     rssiReadings = calloc(MAXIMUM_RSSI_READINGS, sizeof(uint8_t));
 
@@ -296,6 +302,8 @@ void explore(void) {
 }
 
 void returnToBase(void) {
+
+    // Rssi rolling average (Conclusion: 35)
     rssiReadings[rssiIndex] = rssiReading;
     rssiIndex = (rssiIndex + 1) % MAXIMUM_RSSI_READINGS;
 
@@ -307,9 +315,11 @@ void returnToBase(void) {
             rssiValuesSum += rssiReadings[i];
         }
     }
-    uint8_t rssiAverage = rssiValuesSum / nRssiValues;
-    DEBUG_PRINT("Rssi average: %i\n", rssiAverage);
+
+    // uint8_t rssiAverage = rssiValuesSum / nRssiValues;
+    // DEBUG_PRINT("Rssi average: %i\n", rssiAverage);
     // TODO: Add RSSI ? (rssiLandingThreashold)
+
     // If returned to base, land
     static const double distanceToReturnEpsilon = 0.005;
     if (fabs((double)initialPosition.x - (double)positionReading.x) < distanceToReturnEpsilon &&
@@ -320,13 +330,13 @@ void returnToBase(void) {
     }
 
     switch (returningState) {
-    case RETURNING_RETURN: {
+    case RETURNING_ROTATE_TOWARDS_BASE: {
         droneStatus = STATUS_FLYING;
 
         // Calculate rotation angle to turn towards base
         targetYawToBase = atan2(initialPosition.y - positionReading.y, initialPosition.x - positionReading.x) * 360.0 / (2.0 * M_PI);
-        DEBUG_PRINT("Target Yaw: %f\n", (double)targetYawToBase);
-        DEBUG_PRINT("Current yaw: %f\n", (double)yawReading);
+        // DEBUG_PRINT("Target Yaw: %f\n", (double)targetYawToBase);
+        // DEBUG_PRINT("Current yaw: %f\n", (double)yawReading);
 
         // If the drone is towards its base
         static const double yawEpsilon = 5;
@@ -334,22 +344,7 @@ void returnToBase(void) {
         DEBUG_PRINT("Yaw difference: %f\n", yawDifference);
         if (yawDifference < yawEpsilon || yawDifference > (360.0 - yawEpsilon)) {
             DEBUG_PRINT("I'm done turning!\n");
-
-            // Go to explore algorithm when a wall is detected in front of the drone
-            // TODO: Change name
-            static const uint16_t EDGE_DETECTED_THRESHOLD_RETURN = 800;
-            if (frontSensorReading < EDGE_DETECTED_THRESHOLD_RETURN) {
-                // update way point might be useless
-                // targetHeight += EXPLORATION_HEIGHT;
-                // updateWaypoint();
-                returningState = RETURNING_ROTATE;
-            } else {
-                // Go to base using absolute positions
-                setPoint.mode.x = modeAbs;
-                setPoint.mode.y = modeAbs;
-                setPoint.position.x = initialPosition.x;
-                setPoint.position.y = initialPosition.y;
-            }
+            returningState = RETURNING_RETURN;
         } else {
             // Turn drone towards its base
             DEBUG_PRINT("I need to rotate\n");
@@ -359,46 +354,75 @@ void returnToBase(void) {
             setPoint.attitude.yaw = targetYawToBase;
         }
     } break;
+    case RETURNING_RETURN: {
+        droneStatus = STATUS_FLYING;
+
+        // Go to explore algorithm when a wall is detected in front of the drone
+        // TODO: Change name
+        static const uint16_t EDGE_DETECTED_THRESHOLD_RETURN = 800;
+        if (frontSensorReading < EDGE_DETECTED_THRESHOLD_RETURN || returnWatchdog == 0) {
+            DEBUG_PRINT("Obstacle. Rotating (in return)\n");
+            // Reset counters
+            returnWatchdog = MAXIMUM_RETURN_TICKS;
+            returningState = RETURNING_ROTATE;
+        } else {
+            // Go to base using absolute positions
+            setPoint.mode.x = modeAbs;
+            setPoint.mode.y = modeAbs;
+            setPoint.position.x = initialPosition.x;
+            setPoint.position.y = initialPosition.y;
+            returnWatchdog--;
+        }
+    } break;
     case RETURNING_ROTATE: {
         droneStatus = STATUS_FLYING;
-        DEBUG_PRINT("Obstacle. Rotating\n");
 
         if (rotate()) {
+            DEBUG_PRINT("Finished rotating. Going to explore\n");
             returningState = RETURNING_FORWARD;
         }
     } break;
     case RETURNING_FORWARD: {
         droneStatus = STATUS_FLYING;
-        DEBUG_PRINT("Forward \n");
+        // DEBUG_PRINT("Forward \n");
 
         // Obstacle has been passed go back to returning with absolute positions
         static const uint16_t OPEN_SPACE_THRESHOLD = 300;
         if ((rightSensorReading > EDGE_DETECTED_THRESHOLD + OPEN_SPACE_THRESHOLD && obstacleClearedCounter == 0) || exploreWatchdog == 0) {
+            // Generation of a random explore watchdog between 200 and 600
+            static const uint16_t SCOPE_EXPLORE_WATCHDOG = 400;
+            static const uint16_t MINIMUM_EXPLORE_WATCHDOG = 200;
+            maximumExploreTicks = rand() % SCOPE_EXPLORE_WATCHDOG + MINIMUM_EXPLORE_WATCHDOG;
+
             // Reset counters
-            exploreWatchdog = MAXIMUM_EXPLORE_TICKS;
+            DEBUG_PRINT("Obstacle has been passed\n");
+            exploreWatchdog = maximumExploreTicks;
             obstacleClearedCounter = STABILIZE_READING_TICKS;
+
             returningState = RETURNING_RETURN;
             break;
         }
 
         if (!forward()) {
             obstacleClearedCounter = STABILIZE_READING_TICKS;
+            DEBUG_PRINT("Obstacle. Rotating (in forward)\n");
             returningState = RETURNING_ROTATE;
+        } else {
+            // Reset right sensor reading counter if obstacle on the right is detected
+            if (rightSensorReading > EDGE_DETECTED_THRESHOLD) {
+                obstacleClearedCounter--;
+            } else {
+                obstacleClearedCounter = STABILIZE_READING_TICKS;
+            }
         }
         exploreWatchdog--;
-
-        // Reset right sensor reading counter if obstacle on the right is detected
-        if (rightSensorReading > EDGE_DETECTED_THRESHOLD) {
-            obstacleClearedCounter--;
-        } else {
-            obstacleClearedCounter = STABILIZE_READING_TICKS;
-        }
 
     } break;
     case RETURNING_LAND: {
         droneStatus = STATUS_LANDING;
 
         if (land()) {
+            DEBUG_PRINT("Landed\n");
             returningState = RETURNING_IDLE;
         }
     } break;

b724200dcb350ca86b1409d23e49a79de9b2c3c3
Nathanael Beaudoin-Dion <nathanael.beaudoin-dion@polymtl.ca>
2021-04-03 20:40:38 -0400

    Undo remove of set_orientation

--- server/server/map_generator.py
+++ server/server/map_generator.py
@@ -17,6 +17,9 @@ class MapGenerator:
 
         self._web_socket_server.bind('connect', self._web_socket_connect_callback)
 
+    def set_orientation(self, drone_id: str, orientation: Orientation):
+        self._last_orientations[drone_id] = orientation
+
     def set_position(self, drone_id: str, position: Point):
         self._last_positions[drone_id] = position
         self._web_socket_server.send_message('drone-position', {'droneId': drone_id, 'position': position})

033c371a7cef90e07921019ae7cedc2b44ef51e9
Misha Krieger-Raynauld <mkriegerraynauld@gmail.com>
2021-04-06 13:00:03 -0400

    Add preliminary socket_event.h string conversion implementation

--- argos/experiments/socket_event.h
+++ argos/experiments/socket_event.h
@@ -1,7 +1,8 @@
-#include <string>
-
 #ifndef SOCKET_EVENT_H
 #define SOCKET_EVENT_H
+
+#include <string>
+#include <unordered_map>
 
 enum class SocketEvent {
     DroneIds,
@@ -15,39 +16,57 @@ enum class SocketEvent {
     Console,
 };
 
+std::unordered_map<SocketEvent, std::string> socketEventStrings = {
+    {SocketEvent::DroneIds, "drone-ids"},
+    {SocketEvent::BatteryLevel, "battery-level"},
+    {SocketEvent::Orientation, "orientation"},
+    {SocketEvent::Position, "position"},
+    {SocketEvent::Velocity, "velocity"},
+    {SocketEvent::Range, "range"},
+    {SocketEvent::Rssi, "rssi"},
+    {SocketEvent::DroneStatus, "drone-status"},
+    {SocketEvent::Console, "console"},
+};
+
 // TODO: Better way to do this?
-const std::string eventToString(const SocketEvent& event) {
-    switch (event) {
-    case SocketEvent::DroneIds:
-        return "drone-ids";
-        break;
-    case SocketEvent::BatteryLevel:
-        return "battery-level";
-        break;
-    case SocketEvent::Orientation:
-        return "orientation";
-        break;
-    case SocketEvent::Position:
-        return "position";
-        break;
-    case SocketEvent::Velocity:
-        return "velocity";
-        break;
-    case SocketEvent::Range:
-        return "range";
-        break;
-    case SocketEvent::Rssi:
-        return "rssi";
-        break;
-    case SocketEvent::DroneStatus:
-        return "drone-status";
-        break;
-    case SocketEvent::Console:
-        return "console";
-        break;
-    default:
-        return "";
+const std::string& eventToString(const SocketEvent& event) {
+    auto it = socketEventStrings.find(event);
+    if (it !== socketEventStrings.end()) {
+        return it->second;
     }
+    return "Unknown";
+
+    // switch (event) {
+    // case SocketEvent::DroneIds:
+    //     return "drone-ids";
+    //     break;
+    // case SocketEvent::BatteryLevel:
+    //     return "battery-level";
+    //     break;
+    // case SocketEvent::Orientation:
+    //     return "orientation";
+    //     break;
+    // case SocketEvent::Position:
+    //     return "position";
+    //     break;
+    // case SocketEvent::Velocity:
+    //     return "velocity";
+    //     break;
+    // case SocketEvent::Range:
+    //     return "range";
+    //     break;
+    // case SocketEvent::Rssi:
+    //     return "rssi";
+    //     break;
+    // case SocketEvent::DroneStatus:
+    //     return "drone-status";
+    //     break;
+    // case SocketEvent::Console:
+    //     return "console";
+    //     break;
+    // default:
+    //     return "";
+    // }
 }
 
 // static const char* eventToString[] = {

768ccfb39ec279285b400c01d902ce14d627c0d0
Yasmine Moumou <yasmine.moumou@polymtl.ca>
2021-04-06 11:43:30 -0400

    Add socket event enum

---
+++ server/server/sockets/socket_event.py
@@ -0,0 +1,20 @@
+import enum
+
+
+class SocketEvent(enum.Enum):
+    Connect = 'connect'
+    Disconnect = 'disconnect'
+    DroneIds = 'drone-ids'
+    BatteryLevel = 'battery-level'
+    Orientation = 'orientation'
+    Position = 'position'
+    Velocity = 'velocity'
+    Range = 'range'
+    Rssi = 'rssi'
+    DroneStatus = 'drone-status'
+    Log = 'log'
+    Console = 'console'
+    MissionState = 'mission-state'
+    SetLed = 'set-led'
+    MapPoints = 'map-points'
+    ClearMap = 'clear-map'

004bd5f3674fe771e78c5440959a072bab07cbd2
Samer Massaad <samer.massaad@gmail.com>
2021-04-08 17:04:36 -0400

    Format code

--- drone/app_api/src/app_main.c
+++ drone/app_api/src/app_main.c
@@ -175,10 +175,9 @@ void appMain(void) {
         targetYawRate = 0.0;
         targetYawToBase = 0.0;
 
-        const bool shouldNotBroadcastPosition =
-            (missionState == MISSION_STANDBY)
-            || (missionState == MISSION_EXPLORING && (exploringState == EXPLORING_IDLE || exploringState == EXPLORING_LIFTOFF))
-            || (missionState == MISSION_EMERGENCY && emergencyState == EMERGENCY_IDLE);
+        const bool shouldNotBroadcastPosition = (missionState == MISSION_STANDBY) ||
+                                                (missionState == MISSION_EXPLORING && (exploringState == EXPLORING_IDLE || exploringState == EXPLORING_LIFTOFF)) ||
+                                                (missionState == MISSION_EMERGENCY && emergencyState == EMERGENCY_IDLE);
 
         static const uint8_t broadcastProbabilityPercentage = 5;
         if (!shouldNotBroadcastPosition && (rand() % 100) < broadcastProbabilityPercentage) {

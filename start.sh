3652419827eeb91c2189cfba50b59223258b75bb
Misha Krieger-Raynauld <mkriegerraynauld@gmail.com>
2021-04-08 13:15:55 -0400

    Add ARGoS service to docker-compose.yml and create Bash script for x11docker

---
+++ start.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Shell script necessary to initialize x11docker before starting containers
+# with Docker Compose. See https://github.com/mviereck/x11docker/issues/227
+
+function exit_handler() {
+    echo Cleaning up x11docker
+    x11docker --cleanup --quiet
+}
+
+trap exit_handler EXIT
+
+echo Initializing x11docker
+read x_env < <(x11docker --hostdisplay --showenv --quiet)
+echo $x_env
+export $x_env
+
+echo Running Docker Compose
+docker-compose up

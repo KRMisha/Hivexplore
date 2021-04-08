418c71df4bcd81e1d43016c1d07fdde36c40dcb2
Yasmine Moumou <yasmine.moumou@polymtl.ca>
2021-04-08 19:32:23 -0400

    Add missing methods

--- drone/app_api/src/app_main.h
+++ drone/app_api/src/app_main.h
@@ -51,15 +51,18 @@ void emergencyLand(void);
 bool liftoff(void);
 bool forward(void);
 bool rotate(void);
+bool rotateTowardsTargetYaw(void);
 bool land(void);
 bool isCrashed(void);
 void avoidDrone();
 
 void broadcastPosition();
 void p2pReceivedCallback(P2PPacket* packet);
+//void saveLatestP2PContents();
 
 void updateWaypoint(void);
 uint16_t calculateDistanceCorrection(uint16_t obstacleThreshold, uint16_t sensorReading);
+double calculateAngleAwayFromCenterOfMass();
 float calculateVectorLength(vector_t vector);
 
 #endif

126a18b3bf9129cc5ca3eeffb8a487deb529e19f
Yasmine Moumou <yasmine.moumou@polymtl.ca>
2021-04-08 11:23:25 -0400

    Change conversion enum to string

--- argos/experiments/socket_event.h
+++ argos/experiments/socket_event.h
@@ -28,70 +28,13 @@ std::unordered_map<SocketEvent, std::string> socketEventStrings = {
     {SocketEvent::Console, "console"},
 };
 
-// TODO: Better way to do this?
-const std::string& eventToString(const SocketEvent& event) {
+// TODO: place function in cpp file
+const std::string eventToString(const SocketEvent& event) {
     auto it = socketEventStrings.find(event);
-    if (it !== socketEventStrings.end()) {
+    if (it != socketEventStrings.end()) {
         return it->second;
     }
     return "Unknown";
-
-    // switch (event) {
-    // case SocketEvent::DroneIds:
-    //     return "drone-ids";
-    //     break;
-    // case SocketEvent::BatteryLevel:
-    //     return "battery-level";
-    //     break;
-    // case SocketEvent::Orientation:
-    //     return "orientation";
-    //     break;
-    // case SocketEvent::Position:
-    //     return "position";
-    //     break;
-    // case SocketEvent::Velocity:
-    //     return "velocity";
-    //     break;
-    // case SocketEvent::Range:
-    //     return "range";
-    //     break;
-    // case SocketEvent::Rssi:
-    //     return "rssi";
-    //     break;
-    // case SocketEvent::DroneStatus:
-    //     return "drone-status";
-    //     break;
-    // case SocketEvent::Console:
-    //     return "console";
-    //     break;
-    // default:
-    //     return "";
-    // }
 }
-
-// static const char* eventToString[] = {
-//     "drone-ids",
-//     "battery-level",
-//     "orientation",
-//     "position",
-//     "velocity",
-//     "range",
-//     "rssi",
-//     "drone-status",
-//     "console",
-// };
-
-// std::map<SocketEvent, const char*> EventToString;
-// map_init(EventToString)
-//     (SocketEvent::DroneIds, "drone-ids")
-//     (SocketEvent::BatteryLevel, "battery-level")
-//     (SocketEvent::Orientation, "orientation")
-//     (SocketEvent::Position, "position")
-//     (SocketEvent::Velocity, "velocity")
-//     (SocketEvent::Range, "range")
-//     (SocketEvent::Rssi, "rssi")
-//     (SocketEvent::DroneStatus, "drone-status")
-//     (SocketEvent::Console, "console")
-//     ;
 
 #endif

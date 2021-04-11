3af43b012c50c8cca3acbb2a3d74097a911dcd88
Misha Krieger-Raynauld <mkriegerraynauld@gmail.com>
2021-04-11 01:02:54 -0400

    Add quotes around echo statements

--- start.sh
+++ start.sh
@@ -4,18 +4,17 @@
 # with Docker Compose. See https://github.com/mviereck/x11docker/issues/227
 
 function exit_handler() {
-    echo Cleaning up x11docker
+    echo "Cleaning up x11docker"
     x11docker --cleanup --quiet
 }
 
 trap exit_handler EXIT
 
-echo Initializing x11docker
+echo "Initializing x11docker"
 read x_env < <(x11docker --hostdisplay --showenv --quiet)
-echo $x_env
 export $x_env
 
-echo Running Docker Compose
+echo "Running Docker Compose"
 docker-compose -f docker-compose.yml -f docker-compose.argos.yml up
 
 # TODO: Arguments

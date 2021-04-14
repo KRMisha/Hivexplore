4a58cd29a7b3a3c8b21b7381965b4fce52736651
Misha Krieger-Raynauld <mkriegerraynauld@gmail.com>
2021-04-14 00:26:29 -0400

    Remove comment at the top of app_main.c

--- drone/app_api/src/app_main.c
+++ drone/app_api/src/app_main.c
@@ -1,31 +1,3 @@
-/**
- * ,---------,       ____  _ __
- * |  ,-^-,  |      / __ )(_) /_______________ _____  ___
- * | (  O  ) |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
- * | / ,--´  |    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
- *    +------`   /_____/_/\__/\___/_/   \__,_/ /___/\___/
- *
- * Crazyflie control firmware
- *
- * Copyright (C) 2020 Bitcraze AB
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, in version 3.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- *
- * api_app.c - App layer application that calls app API functions to make
- *             sure they are compiled in CI.
- */
-
 #include <math.h>
 #include <stdlib.h>
 #include "app.h"
@@ -44,6 +16,8 @@
 #include "app_main.h"
 
 #define DEBUG_MODULE "APPAPI"
+
+#define DISABLE_FLIGHT true
 
 // Min helper macro
 #define MIN(a, b) ((a < b) ? a : b)
@@ -260,8 +234,10 @@ void appMain(void) {
             memset(&setPoint, 0, sizeof(setpoint_t));
         }
 
-        static const uint8_t TASK_PRIORITY = 3;
-        commanderSetSetpoint(&setPoint, TASK_PRIORITY);
+        #if !DISABLE_FLIGHT
+            static const uint8_t TASK_PRIORITY = 3;
+            commanderSetSetpoint(&setPoint, TASK_PRIORITY);
+        #endif
     }
 }
 
